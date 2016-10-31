c65f944ebdb97016c3353267a4aa993c8b7f3e82
Himanshu Maurya <himanshumaurya2214225@hotmail.com>
2016-11-01 00:28:49 +0530

    Created a component

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,7 +1,26 @@
 'use strict';
 
 angular.module('blogList').
-    controller('BlogListController',function($scope){
-        console.log("hello")
-        $scope.title="Hi There"
+    // this blogList will be used as tag blog-list in index.html
+    component('blogList',{
+        template:  '<div class=""><h1>{{ title }}</h1><button ng-click="someClickTest()">Click Me!</button></div>',
+        controller: function($scope){
+            $scope.title="Hi There"
+            $scope.clicks = 0
+            $scope.someClickTest = function () {
+            console.log("Clicked")
+            $scope.clicks += 1 
+            $scope.title = "Clicked " + $scope.clicks + " times!"
+        }
+    }
     });
+    // controller('BlogListController',function($scope){
+    //     console.log("hello")
+    //     $scope.title="Hi There"
+    //     $scope.clicks = 0
+    //     $scope.someClickTest = function () {
+    //         console.log("Clicked")
+    //         $scope.clicks += 1 
+    //         $scope.title = "Clicked " + $scope.clicks + " times!"
+    //     }
+    // });
